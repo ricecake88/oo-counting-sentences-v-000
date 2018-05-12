ba2171cc8cb126cf59edc9921e21057c4acef343
Grace Shih <vocalise@gmail.com>
2018-05-12 23:05:50 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    questionArray = self.split("\s|!|?|.")
+    questionArray = self.split(/[!,? ])
     binding.pry
     #periodArray = self.split(".")
     #exclamationArray = self.split("!")
