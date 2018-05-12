0256ca70fc6f17a58a6cb9530da463d1a72ac49d
Grace Shih <vocalise@gmail.com>
2018-05-12 23:12:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+   if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+   if self.end_with?("?")
+     true
+   else
+     false
+   end
   end
 
   def exclamation?
-
+   if self.end_with?("!")
+     true
+   else
+     false
+   end
   end
 
   def count_sentences
-
+    questionArray = self.split(/[!.?]/)
+    questionArray.reject!(&:empty?)
+    questionArray.length
   end
 end
+
+str = String.new
+str = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+puts str.count_sentences
